a6b9271e1a0c5086a15a763776f588bd742bdf3e
bird664 <163318982+bird664@users.noreply.github.com>
2024-12-20 15:21:50 +0700

    Not Found

---
+++ frontend/src/components/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './notFound.module.css';
+import { Link } from 'react-router-dom';
+export default function NotFound({ 
+  message = 'Nothing Found!', 
+  linkRoute = '/', 
+  linkText = 'Go To Home Page' 
+ }) {
+  return (
+    <div className={classes.container}>
+      {message}
+      <Link to={linkRoute}>{linkText}</Link>
+    </div>
+  );
+}
+
+NotFound.defaultProps = {
+  message: 'Nothing Found!',
+  linkRoute: '/',
+  linkText: 'Go To Home Page',
+};
